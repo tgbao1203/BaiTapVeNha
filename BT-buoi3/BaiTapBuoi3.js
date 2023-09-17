e835b8df84b86e2ac529ae4484e9324d6bf621cb
Gia Bao <bao.tg1203@outlook.com>
2023-09-17 10:22:49 +0700

    Bai 2,3 Buoi 3

--- BT-buoi3/BaiTapBuoi3.js
+++ BT-buoi3/BaiTapBuoi3.js
@@ -1,3 +1,18 @@
 // Bài 1: Tạo biến height và gán cho nó một giá trị số thể hiện chiều cao
 const height = 162;
 console.log(height + " cm");
+
+// Bài 2: Tạo 1 comment 1 dòng và 1 comment từ 2 dòng trở lên 
+// comment 1 dòng
+/**comment 2 dòng
+ * 2 dòng comment
+ */
+
+/**Bài 3:   Thực hiện log ra cửa sổ console dòng chữ sau: 
+* “Chieu cao cua toi la <height>” 
+* Lưu ý: <height> là biến height vừa tạo ở bài 1 
+*/
+
+console.log("Chieu cao cua toi la" + height);
+
+ 
