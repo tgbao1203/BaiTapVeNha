ddc3c9a34ad4e5aff1dd339a3b4b983a35db71e2
Gia Bao <bao.tg1203@outlook.com>
2023-09-17 12:39:00 +0700

    Nop full 8 bai tap cua buoi 3

--- BT-buoi3/BaiTapBuoi3.js
+++ BT-buoi3/BaiTapBuoi3.js
@@ -13,6 +13,49 @@ console.log(height + " cm");
 * Lưu ý: <height> là biến height vừa tạo ở bài 1 
 */
 
-console.log("Chieu cao cua toi la" + height);
+console.log("Chieu cao cua toi la " + height + " cm");
 
- 
+// Bài 4: Cho biến weight có giá trị bằng 20, hãy tạo biến nextWeight có giá trị nhỏ hơn biết weight 3 đơn vị (dùng toán tử số học) 
+let weight = 20;
+let nextWeight = weight - 3;
+
+// Bài 5: Sử dụng kiến thức về toán tử gán và toán tử số học để tạo các biến a, b, c, d thỏa mãn các yêu cầu sau đây: 
+//a là 1 số dương 
+//b là 1 số âm 
+//c là kết quả của a + b 
+//d là kết quả của a - b 
+
+//let a = 5;
+//let b = 0 - a;
+//let c = a + b;
+//let d = a - b;
+//console.log("Biến a: " + a);
+//console.log("Biến b: " + b);
+//console.log("Biến c: " + c);
+//console.log("Biến d: " + d);
+
+// Bài 6: Cho đề sau, hãy xác định biến b sẽ mang giá trị gì?
+//var a = 1; 
+//var b = ++a * a--; 
+//console.log("Bien b se mang gia tri la: 4");
+//console.log(b);
+
+// Bài 7: Tạo biến fullName (tên đầy đủ) mang giá trị tên đầy đủ, được kết hợp từ giá trị của 2 biến firstName (tên) và lastName (họ). 
+var firstName = "Trinh ";
+var lastName = "Gia Bao";
+var fullName = firstName + lastName;
+console.log("Ten cua toi la: " + fullName);
+
+//Bài 8: Cho đề sau 
+let a = 1; 
+let b = -1; 
+let c = 0; 
+let d = 0; 
+let e = a <= b; 
+let f = c === d; 
+let g = a >= c; 
+console.log("Dap an la: A. false true true");
+ //console.log(e, f, g) 
+ //A. false true true 
+ //B. true false true 
+ //C. false false true 
