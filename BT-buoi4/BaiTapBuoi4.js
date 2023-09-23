65190fa208a70a5fdfa5499105620170627c8a2f
Gia Bao <bao.tg1203@outlook.com>
2023-09-23 11:45:55 +0700

    Nop bai tap buoi 4

---
+++ BT-buoi4/BaiTapBuoi4.js
@@ -0,0 +1,46 @@
+//Bài 1:
+let tuoi = 18; 
+let coTheMuaRuou = tuoi >= 18;
+//console.log(coTheMuaRuou);
+
+//Bài 2:
+/**let a = "";
+let b = 0;
+let c = []; 
+let d = 1 > 2;
+let e = {}; 
+let f = '0';
+console.log("Gia tri falsy la: a, b, d");*/
+
+//Bài 3:
+//let a = "11111";
+//let b = parseInt(a);
+//console.log(a, b);
+
+//Bài 4:
+//let a = 29072015;
+//let b = a.toString();
+//console.log(a, b);
+
+//Bài 5:
+//let x = typeof (10 >= -1 && typeof NaN === "number" && "Ky" && false && true) === false
+console.log("Giá trị của x là: false");
+//console.log(x);
+
+//Bài 6:
+var a = '1';
+var b = 2;
+
+var c = typeof a;
+var d = typeof b;
+var e = typeof d;
+console.log("Giá trị c là: string");
+console.log("Giá trị d là: number");
+console.log("Giá trị e là: string");
+//console.log(c, d, e);
+
+// bai 7
+// x = 1/0 là vô cực Infinity
+const x = 1 / 0;
+
+console.log(x === x + 1);
